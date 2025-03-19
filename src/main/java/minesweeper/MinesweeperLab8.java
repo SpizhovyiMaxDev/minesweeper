49e545780b129156aa1e911f88df597e85b18c94
Maks Spizhovyi <max.spizhovyi@gmail.com>
2025-03-19 13:45:57 -0700

    ImageBox updates

--- src/main/java/minesweeper/MinesweeperLab8.java
+++ src/main/java/minesweeper/MinesweeperLab8.java
@@ -18,15 +18,13 @@ import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Priority;
 import javafx.scene.layout.VBox;
-import javafx.scene.text.Font;
-import javafx.scene.text.FontWeight;
 import javafx.stage.Stage;
 
 
 import java.net.URL;
 import java.util.*;
 
-public class MinesweeperLab7 extends Application {
+public class MinesweeperLab8 extends Application {
     VBox root = new VBox();
 
     private int cellSize = 20;
